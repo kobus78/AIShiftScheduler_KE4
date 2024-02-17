6939d3ba8185ca7d65bc3dd3ea2aa56d11749b1f
Kobus Esterhuysen <kobus78@gmail.com>
2024-02-17 06:05:01 -0800

    Add `say_hello`

--- AIShiftScheduler_KE4/_modidx.py
+++ AIShiftScheduler_KE4/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://kobus78.github.io',
                 'git_url': 'https://github.com/kobus78/AIShiftScheduler_KE4',
                 'lib_path': 'AIShiftScheduler_KE4'},
-  'syms': {'AIShiftScheduler_KE4.core': {'AIShiftScheduler_KE4.core.foo': ('core.html#foo', 'AIShiftScheduler_KE4/core.py')}}}
+  'syms': { 'AIShiftScheduler_KE4.core': { 'AIShiftScheduler_KE4.core.foo': ('core.html#foo', 'AIShiftScheduler_KE4/core.py'),
+                                           'AIShiftScheduler_KE4.core.say_hello': ('core.html#say_hello', 'AIShiftScheduler_KE4/core.py')}}}
